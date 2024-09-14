6994ed5a826cc6e58820387140ab9a9be2514cf3
emPeeGee <eempeegee@gmail.com>
2024-09-14 23:29:44 +0300

    🦄 refactor: change app prefix to smt

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ module.exports = tseslint.config(
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'smt',
           style: 'camelCase',
         },
       ],
@@ -28,7 +28,7 @@ module.exports = tseslint.config(
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'smt',
           style: 'kebab-case',
         },
       ],
