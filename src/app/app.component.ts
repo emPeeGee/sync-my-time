3328e73870c713fe9a30303b13eac064f9c2930f
emPeeGee <eempeegee@gmail.com>
2024-09-15 21:19:56 +0300

    ✨ feat: create calendar component and display current month

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { CalendarComponent } from './shared/components/calendar/calendar.component';
 
 @Component({
   selector: 'smt-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'smt-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.css',
+})
+export class CalendarComponent implements OnInit {
+  today = new Date();
+  currentMonth = this.today;
+
+  constructor() {
+    console.log('cons');
+    // this.today.getD;
+  }
+
+  getDaysInCurrentMonth() {
+    // const nextMonth = this.currentMonth.setMonth(this.currentMonth.getMonth() + 1);
+    const year = this.currentMonth.getFullYear();
+    const month = this.currentMonth.getMonth() + 1;
+    const currentDate = new Date(year, month, 0);
+
+    // return [
+    //   ...Array(nextM.getDate()).map(i => ({
+    //     day: i,
+    //   })),
+    // ];
+
+    const firstDayOfTheMonth = new Date(currentDate);
+    firstDayOfTheMonth.setDate(1);
+    console.log('first', firstDayOfTheMonth, getDayIndexMondaySunday(firstDayOfTheMonth));
+    const emptyDays = Array.from({ length: getDayIndexMondaySunday(firstDayOfTheMonth) }, () => ({
+      day: null,
+      name: '',
+    }));
+
+    const monthDays = Array.from({ length: currentDate.getDate() }, (_, i) => {
+      const day = new Date(year, month, i);
+      return { day: i + 1, name: getDayName(day) };
+    });
+
+    const daysRemainToFillRow = (emptyDays.length + monthDays.length) % 7;
+
+    const lastEmptyDays =
+      daysRemainToFillRow === 0
+        ? []
+        : Array.from({ length: 7 - daysRemainToFillRow }, () => ({ name: null, day: null }));
+    console.log(lastEmptyDays);
+
+    return [...emptyDays, ...monthDays, ...lastEmptyDays];
+
+    // return {
+    //   name: nextM.toLocaleDateString('en-US', { weekday: 'long' }),
+    //   day: nextM.getDate(),
+    // };
+  }
+
+  ngOnInit() {
+    console.log('init');
+  }
+}
+
+const getDayIndexMondaySunday = (date: Date) => (date.getDay() === 0 ? 6 : date.getDay() - 1);
+
+function getDayName(day: Date, locale = 'en-US'): string {
+  return day.toLocaleDateString(locale, { weekday: 'long' });
+}
+
+// var dateStr = '05/23/2014';
+// var day = getDayName(dateStr, 'nl-NL'); // Gives back 'Vrijdag' which is Dutch for Friday.
+
+// function getWeekDays(locale)
+// {
+//     var baseDate = new Date(Date.UTC(2017, 0, 2)); // just a Monday
+//     var weekDays = [];
+//     for(i = 0; i < 7; i++)
+//     {
+//         weekDays.push(baseDate.toLocaleDateString(locale, { weekday: 'long' }));
+//         baseDate.setDate(baseDate.getDate() + 1);
+//     }
+//     return weekDays;
+// }
+
+// var weekDays = getWeekDays('nl-NL'); // Gives back { 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'} which are the days of the week in Dutch.
